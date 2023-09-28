0b259bf5fbd2eeddc4bdd317c136d04b3797254f
David Abolade <69032300+A-believer@users.noreply.github.com>
2023-09-28 16:05:50 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import Popup from "./modals/Popup";
+import Homepage from "./pages/Homepage";
 
 
 export default function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/popup" element={<Popup/>} />
+      <Route path="/home" element={<Homepage/>} />
+    </Routes>
   )
 }
